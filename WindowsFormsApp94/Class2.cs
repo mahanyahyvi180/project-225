08352606b2a386842d98edb3e2e3d903625ed8cd
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-19 23:19:02 +0330

    Add files via upload

---
+++ WindowsFormsApp94/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp94
+{
+  
+    public  delegate double mydelegate(int x);
+
+    public delegate void delbig();
+}
---
+++ WindowsFormsApp94/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp94
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cEmployee t = new cEmployee();
+            t.toobig += t_toobig;
+            t.FirstName = textBox1.Text;
+            t.lastName = textBox2.Text;
+
+        }
+        private void t_toobig()
+        {
+            listBox1.Items.Add("Event Fire");
+        }
+    }
+}
